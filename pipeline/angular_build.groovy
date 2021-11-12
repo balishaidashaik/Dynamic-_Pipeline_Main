070ffffc5ceb89bbd225a6abe089b11b7f53b595
balishaidashaik <91331180+balishaidashaik@users.noreply.github.com>
2021-11-12 20:46:40 +0530

    Update angular_build.groovy

--- pipeline/angular_build.groovy
+++ pipeline/angular_build.groovy
@@ -5,19 +5,52 @@ timeout(5) {
       echo("${APP_URL} Repository was successfully cloned.")
     }	
 	
-    stage("Build Node Modules") {
+  /*  stage("Build Node Modules") {
       nodejs('Node') {
       sh 'npm install'
     }
       echo("Node Modules installed successully")
     }
 	
-    stage("Build/Package the Angular Application") {
-      nodejs('Node') {
+    //stage("Build/Package the Angular Application") {
+      //nodejs('Node') {
       sh 'npm run build'
     }
       echo("AngularJs Application is Built Successfully")
+    }*/
+
+    stage('NPM Install') {
+        withEnv(["NPM_CONFIG_LOGLEVEL=warn"]) {
+            sh 'npm install'
+        }
     }
 
-  }
+    stage('Test') {
+        withEnv(["CHROME_BIN=/usr/bin/chromium-browser"]) {
+          sh 'ng test --progress=false --watch false'
+        }
+        junit '**/test-results.xml'
+    }
+
+    stage('Lint') {
+        sh 'ng lint'
+    }
+
+    stage('Build') {
+        milestone()
+        sh 'ng build --prod --aot --sm --progress=false'
+    }
+
+    stage('Archive') {
+        sh 'tar -cvzf dist.tar.gz --strip-components=1 dist'
+        archive 'dist.tar.gz'
+    }
+
+    stage('Deploy') {
+        milestone()
+        echo "Deploying..."
+    }
+
+
+}
 }
