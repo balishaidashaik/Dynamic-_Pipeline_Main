e8c31df418bc3dd73029d1c82998998c121035e7
balishaidashaik <91331180+balishaidashaik@users.noreply.github.com>
2021-12-17 20:19:46 +0530

    Update react_build.groovy

--- pipeline/react_build.groovy
+++ pipeline/react_build.groovy
@@ -1,23 +1,78 @@
-timeout(5) {
-  node("master") {
-    stage("Code Check Out") {
-      git branch: 'main', credentialsId: env.Credential_ID, url: 'https://github.com/balishaidashaik/Dynamic-pipeline.git'
-      echo("${GIT_URL} Repository was successfully cloned.")
+pipeline {
+     agent any
+     stages{
+       stage("Code Check Out") {
+        steps {
+         git branch: 'main', credentialsId: env.Credential_ID, url: '${APP_URL}'
+         echo("${APP_URL} Repository was successfully cloned.")
     }
+   }
 	
-    stage("Build Node Modules") {
-      //nodejs('Node') {
-      //sh 'npm install'
-   // }
+   stage("Build Node Modules") {
+     steps{
+        nodejs('Node') {
+        bat 'npm install'
+    }
+        echo("Node Modules installed successully")
+   }
+ }
+   stage("Build/Package the Angular Application") {
+     steps{
+        nodejs('Node') {
+        bat 'npm run build'
+    }
+   }
+  }
+   stage("Test the Angular Application") {
+     steps{
+        nodejs('Node') {
+        bat 'npm test'
+    }
+   }
+  }
+	
+    /*stage("Build Node Modules") {
+      nodejs('Node') {
+      sh 'npm install sonarqube-scanner'
+    }
       echo("Node Modules installed successully")
     }
 	
-    stage("Build/Package the React Application") {
-      //nodejs('Node') {
-      //sh 'npm run build'
-   // }
-      echo("React Application is Built Successfully")
+    stage("Build/Package the Angular Application") {
+      nodejs('Node') {
+      sh 'ng build'
     }
+    stage("Test the Angular Application") {
+      nodejs('Node') {
+      sh 'ng test'
+    }
+    stage ('Code Analysis') {
+      nodejs('Node') {
+      sh 'npm run sonar'
+    }
+   }
 
-  }
+	    
+    stage ("publish to s3") {
+      step ([
+        $class: 'S3BucketPublisher',
+        entries: [[
+          sourceFile: '',
+          bucket: 'dynamics3bucket',
+          selectedRegion: 'us-east-1',
+          noUploadOnFailure: true,
+          managedArtifacts: true,
+          //flatten: true,
+          showDirectlyInBrowser: true,
+          keepForever: true,
+        ]],
+        profileName: 'Dynamic-DevOps-Pipeline-Jenkins-S3',
+        dontWaitForConcurrentBuildCompletion: false,
+      ])
+      echo("AngularJs Application is Built Successfully")
+     }
+   }
+  } */
+	 
+ }
 }
