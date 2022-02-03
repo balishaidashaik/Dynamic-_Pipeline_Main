2f54e3f924f52cd256703c499504c2a35de587dd
balishaidashaik <91331180+balishaidashaik@users.noreply.github.com>
2022-02-03 12:17:18 +0530

    Update react_build.groovy

--- pipeline/react_build.groovy
+++ pipeline/react_build.groovy
@@ -1,83 +1,70 @@
 pipeline {
-    agent any 
-
-    stages {
-
-        stage("Code Check Out") {
-            steps {
-                git branch: 'main', credentialsId: env.Credential_ID, url: '${APP_URL}'
-                echo("${APP_URL} Repository was successfully cloned.")
-            }
-        }
+	agent any
+	  
+     
+     stages{
+       stage("Code Check Out") {
+        steps {
+         git branch: 'main', credentialsId: env.Credential_ID, url: '${APP_URL}'
+         echo("${APP_URL} Repository was successfully cloned.")
+    }
+   }
 	
-        stage("Install Node Modules") {
-            steps {
-                nodejs('Node') {
-                    sh 'npm install sonarqube-scanner'
-                    echo("Node Modules are installed successully")
-                }
-            }
-        }
-	
-        stage("Build/Package the React Application") {
-            steps {
-                nodejs('Node') {
-                    sh 'npm run build'
-                    echo("Build is completed")
-                }
-            }
-        }
-
-        /*stage ('Code Analysis') {
-            steps {
-                nodejs('Node') {
-                    sh 'npm run sonar'
-                    echo("Code Analysis is success")
-                }
-            }
-        }*/
-
-        stage ('Unit test application') {
-            steps {
-                nodejs('Node') {
-                    echo 'Starting unit test execution'
-                    sh 'npm test'
-                    echo 'Completed unit test execution'
-                }
-            }
-        }
-    
-        stage('Packaging Artifacts') {
+   stage("Build Node Modules") {
+     steps{
+        nodejs('Node') {
+        sh 'npm install'
+    }
+        echo("Node Modules installed successully")
+   }
+ }
+   stage("Build/Package the Angular Application") {
+     steps{
+        nodejs('Node') {
+        sh 'npn run build'
+    }
+   }
+  }
+   stage("Test the Angular Application") {
+     steps{
+        nodejs('Node') {
+        sh 'npm test'
+    }
+   }
+  }
+   stage ('Packaging Artifacts') {
             steps {
                 nodejs('Node') {
                     echo "Started Packaging artifacts"
-                    sh 'zip -r "build.zip" build/'
+                    sh 'zip -r "dist.zip" dist/'
                     echo "Completed Packaging artifacts"
                 }
             }
         }
+   
+    	    
+    stage ("publish to s3") {
+     steps {
+       step ([
+        $class: 'S3BucketPublisher',
+        entries: [[
+          sourceFile: 'dist.zip',
+          bucket: 'dynamic-s3bucket',
+          selectedRegion: 'us-east-2',
+          noUploadOnFailure: true,
+          managedArtifacts: true,
+          //flatten: true,
+          showDirectlyInBrowser: true,
+          keepForever: true,
+        ]],
+        profileName: 'Dynamic-DevOps-Pipeline-Jenkins-S3',
+        dontWaitForConcurrentBuildCompletion: false,
+      ])
+      echo("AngularJs Application is Built Successfully")
+     }
+    }
     
-        stage ("publish artifacts to s3") {
-            steps {
-                step ([
-                    $class: 'S3BucketPublisher',
-                    entries: [[
-                        sourceFile: 'build.zip',
-                        bucket: 'dynamic-s3bucket',
-                        selectedRegion: 'us-east-2',
-                        noUploadOnFailure: true,
-                        managedArtifacts: true,
-                        //flatten: true,
-                        showDirectlyInBrowser: true,
-                        keepForever: true,
-                    ]],
-                    profileName: 'Dynamic-DevOps-Pipeline-Jenkins-S3',
-                    dontWaitForConcurrentBuildCompletion: false,
-                ])
-                echo ("Artifacts are stored in the S3 bucket")
-            }
-        }
-	stage('Publish files to Nexus') {
+    stage('Publish files to Nexus') {
 	    steps {
 		    //sh 'npm login --registry=http://18.223.156.120:8395/repository/npm-private/'
 		    //sh 'npm publish --access public'
@@ -85,14 +72,13 @@ pipeline {
 		    sh 'npm publish'
 	    }
     }
-      
-        /*stage('Publish files to Nexus') {
-            steps {
-                echo "Started uploading artifacts to Nexus repository"
-                nexusArtifactUploader artifacts: [[artifactId: 'react-helloworld', classifier: '', file: 'build.zip', type: 'zip']], credentialsId: '27756e6f-60a1-4a35-bbc8-157d1ea67b68', groupId: 'com.react', nexusUrl: '3.142.21.161:8081', nexusVersion: 'nexus3', protocol: 'http', repository: 'react-repo', version: '1.0.0'
-                echo ('Artifacts are stored in nexus')
+    /*stage('Publish files to Nexus Artifacts') {
+         steps {
+        echo "Started uploading artifacts to Nexus repository"
+        nexusArtifactUploader credentialsId: '2869fc27-b947-42e9-8fc6-3f93fcc8dece', groupId: 'com.angular', nexusUrl: '18.223.156.120:8395/', nexusVersion: 'nexus3', protocol: 'http', repository: 'http://18.223.156.120:8395/repository/npm-private/', version: '3.37.3-02'
+        echo "Completed uploading artifacts to Nexus repository"
             }
         }*/
-    }
+   }
 }
 
